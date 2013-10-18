679586ad3b577da219b41a0c52cc8a9bdb3e9c3f
mharding4967 <mharding4967@gmail.com>
2013-10-17 22:44:03 -0400

    Created Gyro, Accel, and Motor Driver test code

---
+++ Gyro__Accelerometer__and_Motor_Driver_Test_code_p_board/Gyro__Accelerometer__and_Motor_Driver_Test_code_p_board.ino
@@ -0,0 +1,8 @@
+// This code will test the capabilities of Gyro, Accelerometer,
+// and Motor Controllers.
+// The pins are setup, analog readings are read from the accel.
+// and gyro and printed to the serial module.
+// Accordingly, PWM signals are output to motor controller.
+
+
+
